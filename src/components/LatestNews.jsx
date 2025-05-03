2e950d08561107ced79d1e3de7cc5cd4b9c0457e
Samiul821 <smsamiul821@gmail.com>
2025-05-03 20:56:13 +0600

    Show Breaking News Title on Latest News

--- src/components/LatestNews.jsx
+++ src/components/LatestNews.jsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Marquee from "react-fast-marquee";
 
 const LatestNews = () => {
+  const [latestNews, setLatestNews] = useState([]);
+
+  useEffect(() => {
+    fetch("/news.json")
+      .then((res) => res.json())
+      .then((data) => {
+        const todayPick = data.filter((news) => news.others?.is_today_pick);
+        setLatestNews(todayPick);
+      });
+  }, []);
+
   return (
     <div className="flex items-center gap-5 bg-base-200 p-3">
       <p className="text-base-100 bg-secondary px-3 py-2">Latest</p>
 
       <Marquee className="flex gap-5" pauseOnHover={true} speed={60}>
-        <p className="font-semibold">
-        বিদ্যুৎ বিভ্রাটে অচল রাজধানী: গরমে জনদুর্ভোগ চরমে, হাসপাতাল-কলকারখানায় কাজ বন্ধ, লোডশেডিং পরিস্থিতি নিয়ন্ত্রণে জরুরি বৈঠক ডেকেছে সরকার, তবে স্বাভাবিক অবস্থায় ফিরতে সময় লাগবে আরও ৪৮ ঘণ্টা, জানাল বিদ্যুৎ বিভাগ।
-        </p>
+        <div className="flex space-x-4">
+          {latestNews.map((news) => (
+            <p className="font-semibold" key={news.id}>{news.title}</p>
+          ))}
+        </div>
       </Marquee>
     </div>
   );
